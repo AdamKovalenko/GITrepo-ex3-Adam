6ac1fa7845bea30baea5d5183635f37b4e498d72
AdamKovalenko <adamkovalenko@gmail.com>
2025-11-20 23:09:32 +0200

    ex3 update2

--- AdamKovalenko.java
+++ AdamKovalenko.java
@@ -1,6 +1,6 @@
 public class AdamKovalenko {
     public static void main(String[] args) {
-        String name = "Adam";
+        String name = "Maybe i'm not Adam";
 
         for (int i = 0; i < 3; i++) {
             System.out.println(name);
